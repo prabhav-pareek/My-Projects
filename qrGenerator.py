32d8c63739ea37d0307572aab5fb9e426062a6e6
Prabhav Pareek <148680921+prabhav-pareek@users.noreply.github.com>
2024-11-09 01:41:09 +0530

    Delete qrGenerator.py

--- qrGenerator.py
+++
@@ -1,9 +0,0 @@
-# Making QR Code through Python Code
-import pyqrcode
-
-# String which represent the QR code
-url = input(f"Paste the URL - ")
-# Generate QR code
-qr = pyqrcode.create(url)
-# Create and save the png file naming "projectQR.png"
-qr.png("projectQR.png", scale=10)
